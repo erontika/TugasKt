28bf5ac503a629c0f805e146d3f97dbb48152db8
erontika <erontika09@gmail.com>
2016-12-09 10:15:56 +0700

    Add files via upload

---
+++ sensor.ino
@@ -0,0 +1,20 @@
+//Penggunaan sensor asap
+
+const int sensor=  A0;
+int lampu = 13;
+int sensorValue = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(lampu, OUTPUT);
+}
+
+void loop() {
+  sensorValue=analogRead(0);
+  if (sensorValue >= 520){
+    digitalWrite(lampu, HIGH);
+  }
+else {
+  digitalWrite(lampu, LOW);
+ }
+}
